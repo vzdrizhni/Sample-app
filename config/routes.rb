50392d9f6dd844cf8800e8f8904aed8a34759e15
vzdrizhni <vzdrizhni@gmail.com>
2020-04-10 15:41:29 +0500

    Finished static pages (Nikolai URAAA)

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
-  root 'application#hello'
+  root 'static_pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
