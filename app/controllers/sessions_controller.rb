42c5299b9fa2caf08927e93dad5b633769232049
vzdrizhni <vzdrizhni@gmail.com>
2020-05-25 00:24:45 +0500

    signin feature added

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      flash[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+end
