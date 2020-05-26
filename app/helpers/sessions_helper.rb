2da69c8acdad747e2fab21f3f7d234f043409162
vzdrizhni <vzdrizhni@gmail.com>
2020-05-26 13:49:18 +0500

    navbar css fixed

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,18 +3,6 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  def current_user
-    if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
-    elsif (user_id = cookies.encrypted[:user_id])
-      user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
-        log_in user
-        @current_user = user
-      end
-    end
-  end
-
   def logged_in?
     !current_user.nil?
   end
