c8b22e758fe09a52f68d32a446e6ae6c65dd963c
vzdrizhni <vzdrizhni@gmail.com>
2020-05-27 20:56:41 +0500

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,25 +1,34 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+           foreign_key: "follower_id",
+           dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+           foreign_key: "followed_id",
+           dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :create_activation_digest
 
   before_save { self.email = email.downcase }
-  validates :name, presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
-            format: { with: VALID_EMAIL_REGEX },
+  validates :email, presence: true, length: {maximum: 255},
+            format: {with: VALID_EMAIL_REGEX},
             uniqueness: true
   has_secure_password
 
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: {minimum: 6}
 # Returns the hash digest of the given string.
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+
 # Returns a random token.
   def User.new_token
     SecureRandom.urlsafe_base64
@@ -72,12 +81,36 @@ class User < ApplicationRecord
     Micropost.where("user_id = ?", id)
   end
 
+  def follow(other_user)
+    following << other_user
+  end
+
+# Unfollows a user.
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+# Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
+  def feed
+    following_ids = "SELECT followed_id FROM relationships
+      WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+      OR user_id = :user_id", user_id: id)
+  end
+
+
   private
-  # Converts email to all lower-case.
+
+# Converts email to all lower-case.
   def downcase_email
     self.email = email.downcase
   end
-  # Creates and assigns the activation token and digest.
+
+# Creates and assigns the activation token and digest.
   def create_activation_digest
     self.activation_token = User.new_token
     self.activation_digest = User.digest(activation_token)
--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,14 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end
